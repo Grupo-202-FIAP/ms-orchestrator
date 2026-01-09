25909d6c12e9a50d94d795d57ed2e5807fdc9389
leobr1t0 <leonardo70177@gmail.com>
2026-01-08 23:45:25 -0300

    fix: sqs config

--- target/classes/com/nextime/orchestrator/domain/enums/EEventSource.kt
+++
@@ -1,7 +0,0 @@
-package com.nextime.orchestrator.domain.enums
-
-enum class EEventSource(val source: String) {
-    ORCHESTRATOR("ORCHESTRATOR"),
-    PAYMENT("PAYMENT"),
-    PRODUCTION("PRODUCTION"),
-}
